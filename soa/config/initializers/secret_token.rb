50f4c7fadec1e7ea9283154ba097799ac64863be
Jason Clark <jason@jjasonclark.com>
2014-01-13 17:31:02 -0800

    rails new soa

---
+++ soa/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Soa::Application.config.secret_key_base = '4b52a95210d79245710feddc2de835775cd15819606e8d6e4922a3f1e56269d07dd67efa068cd26d43ae11c9d46fd2ea83dbc03502cd9956fb1f4c9178061de8'
---
+++ soa/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Soa::Application.config.session_store :cookie_store, key: '_soa_session'
